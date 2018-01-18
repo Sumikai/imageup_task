287f8746e4cfcbfae0589f38b670c71ac0178ae6
HASHIMOTO Jun <jun840sasanqua@gmail.com>
2018-01-18 12:21:36 +0000

    “bootstrapシリーズ課題2として作成”

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'blogs#index'
+  #get 'twiclones/top'
   
   resources :blogs do
     collection do
       post :confirm
+      get :top
     end
   end
 end
